d3c919ea02849470f130954e60b4678f37da7521
alexfurst1 <143307271+alexfurst1@users.noreply.github.com>
2026-03-16 19:21:55 -0400

    function to upload timestamp feedback to supabase.

--- backend/upload/upload_results.py
+++ backend/upload/upload_results.py
@@ -2,10 +2,27 @@
 from backend.storage.supabase_client import supabase
 
 def upload_general(video_id, ai_output): #llava outputs a string
-    response = (
-        supabase.table('summaries')
-        .insert({'video_id':video_id,'feedback':ai_output})
-        .execute()
+    try:
+        response = (
+            supabase.table('summaries')
+            .insert({'video_id':video_id,'feedback':ai_output})
+            .execute()
+            )
+        return response
+    except Exception as e:
+        print(f'Error uploading general feedback. Error: {e}')
+
+    return None
+
+def upload_specific(video_id, ai_output,start_pos,end_pos):
+    try:
+        response = (
+            supabase.table('timestamps')
+            .insert({'video_id':video_id,'feedback':ai_output,'t_start_seconds':start_pos,'t_end_seconds':end_pos})
+            .execute()
         )
-    print(response)
-    return response
+        return response
+    except Exception as e:
+        print(f'Error uploading timestamped feedback. Error: {e}')
+    
+    return None
